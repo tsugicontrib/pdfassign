de82d24b45cd6e1e658dd8e4728b38b1753c681f
Charles Severance <csev@umich.edu>
2020-03-12 01:50:32 -0400

    Updates

--- index.php
+++ index.php
@@ -49,37 +49,40 @@ $api_endpoint = $CFG->wwwroot . '/api/annotate/' . session_id() . ':' . $LAUNCH-
 
 $matches = array(
     array(false, '</head>','
-<link href="http://localhost:8888/tsugi-static/js/jquery-ui-1.11.4/jquery-ui.min.css" rel="stylesheet">
+<link href="'.$CFG->staticroot.'/js/jquery-ui-1.11.4/jquery-ui.min.css" rel="stylesheet">
 '.Annotate::header().'
 <link rel="stylesheet" href="'.$CFG->staticroot.'/js/annotator-full.1.2.10/annotator.min.css" />
 </head>'),
     array(false, '<div id="page-container">', '
 <div id="page-container">
+<div style="position: fixed; top:10px; right:20px; z-index: 10000;">
+<button onclick="alert(\'To annotate text, highlight text and an annotation input box will pop up.\'+
+\' To view, edit or delete an existing annotation, simply hover over the highlighted text.\');"
+style="border-radius: 4px; border: 4px solid darkblue; color: white; background-color: #008CBA;
+ font-size: 16px; padding: 10px 16px;">'.__('Help').'</button>
 <button onclick="window.location.href = \''.addSession($next).'\';"
-style="position: fixed; border-radius: 4px; border: 4px solid darkblue; z-index: 10000; color: white; background-color: #008CBA;
- font-size: 16px; right:20px;  padding: 10px 16px;">'.$next_text.'</button>
+style="border-radius: 4px; border: 4px solid darkblue; color: white; background-color: #008CBA;
+ font-size: 16px; padding: 10px 16px;">'.$next_text.'</button>
+</div>
 '),
     array(false, '</body>', '
 <script src="'.$CFG->staticroot.'/js/jquery-1.11.3.js"></script>
-<script src="http://localhost:8888/tsugi-static/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+<script src="'.$CFG->staticroot.'/js/jquery-ui-1.11.4/jquery-ui.min.js"></script>
 '.Annotate::footer($user_id).'
-<script type="text/javascript">
-function startAnnotate() {
-      tsugiStartAnnotation("#page-container");
-}
+<script>
 $(document).ready( function () {
-    setTimeout(startAnnotate, 10);
-    console.log("timer started...");
+    tsugiStartAnnotation("#page-container");
 });
 </script>
 </body>'),
 );
 
-function augmentHTML($text, &$matches) {
+function augmentHTML($text, &$matches, $pos) {
     for($i=0; $i<count($matches); $i++) {
         $match = $matches[$i];
         if ( $match[0] ) continue;  // Only match once
         if ( strpos($text, $match[1]) === false ) continue;
+        // error_log("Processing match $i at postion $pos");
         $text = str_replace($match[1], $match[2], $text);
         $matches[$i][0] = true;
     }
@@ -87,19 +90,20 @@ function augmentHTML($text, &$matches) {
 }
 
 if ( is_string($blob) ) {
-    $blob = augmentHTML($blob, $matches);
+    $blob = augmentHTML($blob, $matches, 0);
     echo($blob);
 } else {
     $lastcontent = false;
     $buffer_size = 100000;
     $keep_length = 100;
+    $pos = 0;
     while( $content = fread($blob, $buffer_size) ) {
         if ( $lastcontent == false ) {
             $lastcontent = $content;
             continue;
         }
         $content = $lastcontent . $content;
-        $content = augmentHTML($content, $matches);
+        $content = augmentHTML($content, $matches, $pos);
 
         // Retain the last 6 characters (/head> and no more)
         $len = strlen($content);
@@ -108,12 +112,13 @@ if ( is_string($blob) ) {
             $lastcontent = false;
             continue;
         }
+        $pos = $pos + ($len-$keep_length);
         echo(substr($content, 0, $len-$keep_length));
         $lastcontent = substr($content, $len-$keep_length, $keep_length);
     }
     if ( $lastcontent ) {
         $blob = $lastcontent;
-        $blob = augmentHTML($blob, $matches);
+        $blob = augmentHTML($blob, $matches, -1);
         echo($blob);
     }
 }
